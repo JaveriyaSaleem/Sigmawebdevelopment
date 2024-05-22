f64ca1d95cae37c79d9d2f6f978f3211aabf753c
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-05-22 19:39:52 +0500

    added more

---
+++ variables/app.js
@@ -0,0 +1,8 @@
+// we are using let here as var is global scope and let can be globally scopped or block scopped 
+// We learned about primative now we checking object 
+let student = {
+    studentName: "Javeriya Saleem",
+    studentClass: "12 Grade",
+    studentAge: "19"
+
+}
