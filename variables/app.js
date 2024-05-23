95121f9fa9dbf47db06585c4ecddb352229e51e7
JaveriyaSaleem <javeriyasaleem2@gmail.com>
2024-05-23 23:23:31 +0500

    added chap 1

--- variables/app.js
+++ variables/app.js
@@ -14,3 +14,38 @@ console.log(arr[2]); //that's how we call array
 student.favSubject = "Drawing";
 console.log(student); //updation made 
 console.log(arr[2,3]); //only one working
+// assignment 
+// page 4 
+let studentData ={
+    "Name": "Javeriya",
+    "Phone Number": "03123456780",
+    "Total Marks": "930/1000"
+}
+console.table(studentData);
+// question 1 
+let firstVar = "Hello";
+console.log(firstVar);
+firstVar = 299;
+console.log(firstVar);
+// question 2 
+console.log(typeof (firstVar));
+// question 3 
+const myName = "Javeriya";
+console.log(myName);
+// myName = 233; it didn't updated as it was const and showed error
+console.log(myName);
+// question 4 
+// no const didn't let me updated the value 
+// question 5 
+let dictionary = {
+    "Word": "Meaning",
+    "Magnificant":"something which is extremely beautiful",
+    "Discharge" : "Release",
+    "Yelling": "Shouting in loud, sharp way",
+    "Excemption": "The action of freeing someone from any obligation",
+    "inclined" : "leaning towards someone percepective"
+};
+console.table(dictionary);
+// end 
+let data = ["Toyota", "Porsche","Lamborghini","Hyundai","Ferrari","Mercedez","BMW","Volkswagen","Tesla","Ford","Honda","Nisan","Audi","Jaguar","Lexus","Bently",];
+console.log(data.length);
